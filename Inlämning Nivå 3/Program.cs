8d22a05a12f58e263ef7d4b14932dd798bc60c5e
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-14 20:35:55 +0100

    Add project files.

---
+++ Inlämning Nivå 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Skriv in 5 heltal:");
+        int summa= int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Ange heltal " + i + ": ");
+            int tal = int.Parse(Console.ReadLine());
+            summa += tal; 
+        }
+
+        
+        Console.WriteLine("Summan av de inmatade talen är: " + summa);
+    }
+}
+
